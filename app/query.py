7c33d8478cf202b21c9d00c709103cd45876211f
Michael D. Johnson <mdjohnson44@gmail.com>
2019-12-20 15:42:40 -0600

    updating branch

--- app/query.py
+++ app/query.py
@@ -1,15 +1,20 @@
 import easyaccess as ea
 
-def connect_to_db(section='dessci'):
+def connect_to_db(section='desoper'):
     dbh = ea.connect(section,quiet=True)
     cur= dbh.cursor()
     return cur
 
 
-def query_username(username,lastname,firstname,email):
-    results = ea.do_find_user(username)
-
+def query_username(con,username):
+    #results = ea.do_find_user(username)
+    query = "SELECT count(*) FROM des_users WHERE username={username}".format(username=username)
     if len(results) == 1:
         return True
     else:
         return False
+
+def main(username):
+    con = connect_to_db()
+    is_user = query_username(username)
+    return is_user
--- app/views.py
+++ app/views.py
@@ -1,7 +1,33 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,url_for,redirect,session
+from wtforms import Form,validators, StringField
+from app import app,query
 
-@app.route('/')
-@app.route('/index')
+class EnterText(Form):
+    username = StringField(validators=[validators.InputRequired()])
+
+@app.route('/',methods=['POST','GET'])
+@app.route('/index',methods=['POST','GET'])
 def index():
+    message = None
+    form = EnterText(request.form)
+    if request.method == 'POST':
+        text = request.form['username']
+    else:
+        text = None
+
+    if form.validate():
+        print(text)
+        #exists = query.main(text)
+        if text =='mjohns44':
+            exists = True
+        else:
+            exists = False
+
+        print(exists)
+        return redirect(url_for('form_submitted',user=text,exists = exists))
+    
     return render_template('index.html')
+
+@app.route('/form_submitted/<user>/<exists>')
+def form_submitted(user=None,exists=None):
+    return render_template('form_submitted.html',user=user,exists=exists)
