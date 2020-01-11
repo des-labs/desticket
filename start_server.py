c9800ed45c7db2e73e83a44ce3238f3175e991c2
Michael D. Johnson <michaeldjohnson@control.local>
2020-01-10 18:43:00 -0600

    adding loggin

--- start_server.py
+++ start_server.py
@@ -1,6 +1,8 @@
 from gevent.pywsgi import WSGIServer
 from app import app
+import logging
 
 if __name__=="__main__":
+    logging.basicConfig(filename='server.log',level=logging.DEBUG)
     http_server = WSGIServer(('',5000),app)
     http_server.serve_forever()
