febc008456fa6dda5d2e37c8bfa68e9ec97552a0
Michael D. Johnson <mdjohnson44@gmail.com>
2020-01-13 16:10:15 -0600

    updating rel paths

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 import jinja2
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='desticket-static')
 
 app.config.from_pyfile("../settings.cfg")
 app.jinja_loader = jinja2.FileSystemLoader([app.config["TEMPLATES_PATH"],
--- app/views.py
+++ app/views.py
@@ -32,8 +32,7 @@ class Manual(Form):
     email = StringField(label='email',validators=[validators.InputRequired()])
     unlock = BooleanField(label='unlock',validators=[validators.Optional()])
 
-@app.route('/',methods=['POST','GET'])
-@app.route('/index',methods=['POST','GET'])
+@app.route('/desticket',methods=['POST','GET'])
 def index(message=None):
     form = EnterText(request.form)
     if request.method == 'POST':
@@ -48,7 +47,7 @@ def index(message=None):
     
     return render_template('index.html',message=message)
 
-@app.route('/form_submission',methods=['POST','GET'])
+@app.route('/desticket/form_submission',methods=['POST','GET'])
 def form_submission(user=None,email=None,jira_ticket=None,count=None):
     form = ResetButton(request.form)
     if request.method=='POST':
@@ -89,11 +88,11 @@ def form_submission(user=None,email=None,jira_ticket=None,count=None):
 
     return render_template('form_submission.html',user=user,email = email, count=count,jira_ticket = jira_ticket)
 
-@app.route('/passwd_reset/<user>',methods=['POST','GET'])
+@app.route('/desticket//passwd_reset/<user>',methods=['POST','GET'])
 def passwd_reset(user=None,text=None):
     return render_template('passwd_reset.html',user=user,text=text)
 
-@app.route('/search',methods=['POST','GET'])
+@app.route('/desticket/search',methods=['POST','GET'])
 def search():
     form = Search(request.form)
     if request.method=='POST':
@@ -104,7 +103,7 @@ def search():
             
     return render_template('search.html')
 
-@app.route('/manual_reset/<user>',methods=['POST','GET'])
+@app.route('/desticket/manual_reset/<user>',methods=['POST','GET'])
 def manual_reset(user=None,unlock=True,reset=False):
     form = Manual(request.form)
     if request.method == 'POST':
