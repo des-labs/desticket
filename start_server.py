854796fcef642271f37b8799c3a78ea06f6d58f7
Michael D. Johnson <mdjohnson44@gmail.com>
2020-02-12 14:22:37 -0600

    adding auto reload on changes

--- start_server.py
+++ start_server.py
@@ -1,8 +1,14 @@
 from gevent.pywsgi import WSGIServer
-from app import app
+import werkzeug.serving
 import logging
+from app import app
 
-if __name__=="__main__":
+@werkzeug.serving.run_with_reloader
+def runServer():
+    app.debug = True
     logging.basicConfig(filename='server.log',level=logging.DEBUG)
     http_server = WSGIServer(('',5000),app)
     http_server.serve_forever()
+
+if __name__=="__main__":
+    runServer()
