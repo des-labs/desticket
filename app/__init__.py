3138ef8133e5170d32936aea134f76c7070b84c2
Michael D. Johnson <mdjohnson44@gmail.com>
2020-02-12 16:09:11 -0600

    fixing logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ import os
 
 from flask import Flask
 import jinja2
+import logging
 
 app = Flask(__name__,static_folder='static')
-
 app.config.from_pyfile("../settings.cfg")
 app.jinja_loader = jinja2.FileSystemLoader([app.config["TEMPLATES_PATH"]])
+logging.basicConfig(filename='server.log',level=logging.INFO)
 
 from app import views
--- start_server.py
+++ start_server.py
@@ -1,12 +1,10 @@
 from gevent.pywsgi import WSGIServer
 import werkzeug.serving
-import logging
 from app import app
 
 @werkzeug.serving.run_with_reloader
 def runServer():
     app.debug = True
-    logging.basicConfig(filename='server.log',level=logging.DEBUG)
     http_server = WSGIServer(('',5000),app)
     http_server.serve_forever()
 
