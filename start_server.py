2e69073cbd726a5c2c692d7b6d3f41c5ca1dbf2b
Michael D. Johnson <michaeldjohnson@control.local>
2020-02-19 14:08:44 -0600

    moving runserver code under the if name

--- start_server.py
+++ start_server.py
@@ -1,12 +1,12 @@
-from gevent.pywsgi import WSGIServer
-import werkzeug.serving
 from app import app
 
-@werkzeug.serving.run_with_reloader
-def runServer():
-    app.debug = True
-    http_server = WSGIServer(('',5000),app)
-    http_server.serve_forever()
-
 if __name__=="__main__":
+    from gevent.pywsgi import WSGIServer
+    import werkzeug.serving
+    @werkzeug.serving.run_with_reloader
+    def runServer():
+        app.debug = True
+        http_server = WSGIServer(('',5000),app)
+        http_server.serve_forever()
+
     runServer()
